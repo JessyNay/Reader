12afe050132c9d09ba760b34d989695a9b7967f7
Isaac Sh <424675+Iktwo@users.noreply.github.com>
2018-10-30 23:09:21 -0700

    Add button to open assitant settings

--- app/src/main/java/com/jessynay/reader/MainActivity.kt
+++ app/src/main/java/com/jessynay/reader/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.jessynay.reader
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        settingsButton.setOnClickListener { startActivity(Intent(Settings.ACTION_VOICE_INPUT_SETTINGS)) }
     }
 }
